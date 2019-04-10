a06fdcd7beed8adbb1b49221013856a99e4ed802
Jennifer Kim <jjkim429@gmail.com>
2019-04-10 03:02:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  newarray = []
+  i = 0
 
 
+
+def hamburger(toppings)
+  my_statements = []
+  toppings.each do |topping|
+    my_statements << "I love #{topping} on my burgers!"
+  end
+  my_statements
+end
