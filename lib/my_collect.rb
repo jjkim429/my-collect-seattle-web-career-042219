f598a09ff2ab5d8908e229908e46314c709fb284
Jennifer Kim <jjkim429@gmail.com>
2019-04-10 03:06:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,18 +7,3 @@ def my_collect(array)
   end
   newarray
 end
-
-def hamburger(toppings)
-  my_statements = []
-  toppings.each do |topping|
-    my_statements << "I love #{topping} on my burgers!"
-  end
-  my_statements
-end
-
-i = 0
-  while i < array.length
-    yield(array[i])
-    i = i += 1
-  end
-  array
